205310ef14be45dc8075f2af69c780910a2d3641
Leon De Quadros <leonquadros888@gmail.com>
2026-01-14 21:51:00 +0530

    finished the backend API implementation. Implemented rate limiter with redis; changed the server.js to ensure the DB connects before the app starts.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,22 @@ import notesRoutes from "./routes/notesRoutes.js"
 import express from "express"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
+import rateLimiter from "./middleware/rateLimiter.js"
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 5001
 
 app.use(express.json())
+app.use(rateLimiter)
 
 app.use("/api/notes", notesRoutes)
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
+});
 
 
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
 
